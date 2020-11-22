c1a34082c0b279f131acdf19d53255e1565f3f88
shahil-pixel <71595004+shahil-pixel@users.noreply.github.com>
2020-11-22 21:16:26 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,32 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+           
+            restitution :0.4,
+            friction :0.0,
+            //isStatic:true
+           
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+    
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        if(this.body.speed<3){
+          this.body.Visibility = true;
+        } else{
+          this.body.Visibility = false;
+          tint(255);
+        }
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        pop();
+      }
+}
